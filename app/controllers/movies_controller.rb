5c13a758acc765ed0c7522cabc89877a03cf80d0
fuadAbdiaziz <fuadabdaz99@gmail.com>
2023-07-05 19:50:06 -0700

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,32 @@
 class MoviesController < ApplicationController
-  
+  wrap_parameters format: []
+
   def index
     movies = Movie.all
-    render json: movies
+    render json: toys
+  end
+
+  def create
+    movie = Movie.create(toy_params)
+    render json: toy, status: :created
+  end
+
+  def update
+    movie = Movie.find_by(id: params[:id])
+    render json: toy.update(toy_params)
+  end
+
+  def destroy
+    toy = Movie.find_by(id: params[:id])
+    movie.destroy
+    head :no_content
+  end
+
+  
+  private
+  
+  def toy_params
+    params.permit(:name, :image, :likes)
   end
 
 end
